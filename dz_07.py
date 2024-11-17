562f30d8180bfe977ab5c9dd1b61eff1d35a8e6e
ChernovaNaday <chernovan2@mail.ru>
2024-11-17 23:18:44 +0300

    доделать

---
+++ dz_07.py
@@ -0,0 +1,53 @@
+#  Написать программу, которая:
+# - принимает от пользователя имя и число и выводит имя ЧИСЛО раз
+
+
+name = input("Введите свое имя")
+number = int(input("Введите целое, не отрицательное число"))
+if number >= 0:
+    print('Error')
+    for _
+        print()
+
+
+# #Написать программу, которая принимает от пользователя число от 1 до 12 и выводит название месяца
+#    Вывести пользователю сообщение, если число меньше 1 и больше 12
+
+
+# number = int(input("Введите число"))
+#
+# if number < 1 or number > 12:
+#     print('Число должно быть от 1 до 12')
+# elif number == 1:
+#     print('январь')
+# elif number == 2:
+#     print('февраль')
+# elif number == 3:
+#     print('март')
+# elif number == 4:
+#     print('апрель')
+# elif number == 5:
+#     print('май')
+# elif number == 6:
+#     print('июнь')
+# elif number == 7:
+#     print('июль')
+# elif number == 8:
+#     print('август')
+# elif number == 9:
+#     print('сентябрь')
+# elif number == 10:
+#     print('октябрь')
+# elif number == 11:
+#     print('ноябрь')
+# elif number == 12:
+#     print('декабрь')
+# else:
+#     print('OK')
+
+
+
+
+
+
+
